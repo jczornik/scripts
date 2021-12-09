fb835cb91eafcb502e50f6b2b6f313b090d21c52
jczornik <janusz@czornik.pl>
2021-12-09 23:10:18 +0100

    Extract function for getting all files

--- functions.sh
+++ functions.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
+get_all_files_r() {
+    local base_path=$1
+
+    find $base_path -not -type d | awk -F"$base_path/" '{print $2}'
+}
+
 get_files_to_link () {
     local base_path=$1
 
-    find $base_path -not -type d | awk -F"$base_path/" '{print $2}' | sed 's/^.*install.sh$//g'
+    get_all_files_r $base_path | sed 's/^.*install.sh$//g'
 }
 
 append_to_autostart_awesomewm () {
