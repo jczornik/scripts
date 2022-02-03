0af569363812bc1884a5cc6e8f3c8f243960ca94
Janusz Czornik <janusz@czornik.pl>
2022-02-03 11:54:31 +0100

    Create .local/bin during installation

--- local/install.sh
+++ local/install.sh
@@ -5,6 +5,8 @@ source ../functions.sh
 readonly FILES=$(get_files_to_link $(pwd))
 readonly DST="/home/$(whoami)/.local/bin"
 
+create_path_if_doesnt_exist "$DST"
+
 for file in $FILES; do
     ln -s "$(pwd)/$file" "$DST/$file"
 done
